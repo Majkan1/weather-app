f68ab3e91eea5e89b0ba02f3766fc9c9c169f7f1
Mikołaj Michalak <majkanart117@gmail.com>
2025-12-09 18:39:55 +0100

    Add React Router to the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
-
-function App() {
+import {Routes,Route,Link} from "react-router-dom";
+export default function App() {
   return (
       <>
-      
+        <Main/>
       </>
   )
 }
 
-export default App
+function Main(){
+  return (
+    <>
+      <nav>
+        <Link to = "/">Home</Link>
+        <Link to ="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/" element = {<About/>}/>
+      </Routes>
+    </>
+  )
+}
