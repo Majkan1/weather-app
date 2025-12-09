f8bc6295cf54b33b7e4b67b9220b025542a8e318
Mikołaj Michalak <majkanart117@gmail.com>
2025-12-09 11:56:48 +0100

    Add organization to the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>Pogoda</h1>
-      {/* Tutaj zbudujesz swoją aplikację pogodową od zera */}
-    </div>
+      <>
+      
+      </>
   )
 }
 
