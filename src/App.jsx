5debde4e24cad24c1cb9b8bcd68310b9d0e00742
Mikołaj Michalak <majkanart117@gmail.com>
2025-12-22 07:30:14 +0100

    moved ul to main

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,11 @@ function Main(){
     <div>
       <div className="main">
         <input type = "text" value = {tekst} placeholder = "Write here the town you want to see a weather" onChange = {(e)=>setTekst(e.target.value)}/>
+        <ul>
+          {tekst && array1.map((item,index)=>(
+            <li onClick = {()=> setTekst(item)} key = {index} style = {{cursor:'pointer',color:'white'}}>{item}</li>))}
+        </ul>
       </div>
-      <ul>
-        {tekst && array1.map((item,index)=>(
-          <li onClick = {()=> setTekst(item)} key = {index} style = {{cursor:'pointer',color:'white'}}>{item}</li>))}
-      </ul>
       <button>Search location</button>
     </div>
   )
