d5941ec8337d673196c3b77c2c321ddf1603c725
Mikołaj Michalak <majkanart117@gmail.com>
2025-12-22 18:57:26 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -25,26 +25,57 @@ function Main({tekst,setTekst}){
 }
 
 function Picture({tekst}){
-  const [weather,setWeather] = useState(null);
+  const [weather, setWeather] = useState(null);
+  useEffect(() => {
+    async function Data() {
 
-  useEffect(()=>{
-    async function Data(){
-      try {
-        const url = `https://wttr.in/${encodeURIComponent(tekst)}?format=j1&lang=pl`;
-        const res = await fetch(url);
-        const data = await res.json();
-        setWeather(data);
-      } catch {
+      const geoUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(tekst)}&count=1&language=pl&format=json`;
+      const geoRes = await fetch(geoUrl);
+      const geoData = await geoRes.json();
+
+      const lat = geoData.results.latitude;
+      const lon = geoData?.results?.[0]?.longitude;
+      const name = geoData?.results?.[0]?.name;
+      const admin1 = geoData?.results?.[0]?.admin1;
+      const country = geoData?.results?.[0]?.country;
+      if (lat == null || lon == null) {
         setWeather(null);
+        return;
       }
+
+      const forecastUrl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,wind_speed_10m,precipitation,weather_code&timezone=auto`;
+      const res = await fetch(forecastUrl);
+      const data = await res.json();
+
+      setWeather({
+        placeName: name,
+        admin1,
+        country,
+        ...data,
+      });
     }
-    Data();
-  },[tekst])
-    return (
+      Data();
+    },[tekst]);
+
+  return (
     <>
       {weather && (
-        <div className = "Div">
-          <p>{weather}</p>
+        <div className="Div">
+          <p>
+            City: {weather?.placeName}
+            {weather?.admin1 ? `, ${weather.admin1}` : ''}
+            {weather?.country ? `, ${weather.country}` : ''}
+          </p>
+          <p>
+            Temp: {weather?.current?.temperature_2m}{weather?.current_units?.temperature_2m}
+          </p>
+          <p>
+            Wind speed: {weather?.current?.wind_speed_10m}{weather?.current_units?.wind_speed_10m}
+          </p>
+          <p>
+            Precipitation: {weather?.current?.precipitation}{weather?.current_units?.precipitation}
+          </p>
+          <p>Weather code: {weather?.current?.weather_code}</p>
         </div>
       )}
     </>
