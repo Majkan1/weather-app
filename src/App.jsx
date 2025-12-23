0c3e38e92a517ae0b734dc4e68632cc0879f33b5
Mikołaj Michalak <majkanart117@gmail.com>
2025-12-23 12:08:45 +0100

    Add new design

--- src/App.jsx
+++ src/App.jsx
@@ -86,11 +86,11 @@ function Picture({tekst}){
             {weather?.country ? `, ${weather.country}` : ''}
           </p>
           <p>
-            <img src={new URL('./assets/all/thermometer.svg', import.meta.url).href} alt="temp" style={{width:'20px', height:'20px', marginRight:'5px'}} />
+            <img src={new URL('./assets/all/thermometer.svg', import.meta.url).href} alt="temp" style={{width:'50px', height:'50px', marginRight:'5px'}} />
             {weather?.current?.temperature_2m}{weather?.current_units?.temperature_2m}
           </p>
           <p>
-            <img src={new URL('./assets/all/wind.svg', import.meta.url).href} alt="wind" style={{width:'20px', height:'20px', marginRight:'5px'}} />
+            <img src={new URL('./assets/all/wind.svg', import.meta.url).href} alt="wind" style={{width:'50px', height:'50px', marginRight:'5px'}} />
             {weather?.current?.wind_speed_10m}{weather?.current_units?.wind_speed_10m}
           </p>
         </div>
